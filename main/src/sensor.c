0a0e3c68dd14934f3c6ee050cf3fee60ee40dc48
luxing <luxing9968@gmail.com>
2022-06-12 13:57:17 +0800

    change to PNP switch

--- main/src/sensor.c
+++ main/src/sensor.c
@@ -22,9 +22,15 @@ void switch_sensor_gpio_isr_callback(void *arg)
 
 void InitSwitchSensor(void)
 {
-    gpio_reset_pin(SWITCH_SENSOR_GPIO);
-    gpio_set_direction(SWITCH_SENSOR_GPIO, GPIO_MODE_INPUT);
-    gpio_set_intr_type(SWITCH_SENSOR_GPIO, GPIO_INTR_POSEDGE);
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << SWITCH_SENSOR_GPIO),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = 1,
+        .pull_down_en = 0,
+        .intr_type = GPIO_INTR_NEGEDGE,
+    };
+
+    gpio_config(&io_conf);
     gpio_install_isr_service(0);
     gpio_isr_handler_add(SWITCH_SENSOR_GPIO, switch_sensor_gpio_isr_callback, NULL);
 }
