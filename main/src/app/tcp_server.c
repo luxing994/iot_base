0b5a5a7f94c65459ade66a06f2e314059a18382f
luxing <luxing9968@gmail.com>
2023-03-19 15:06:46 +0800

    fix send_command task bug

--- main/src/app/tcp_server.c
+++ main/src/app/tcp_server.c
@@ -758,7 +758,7 @@ void send_data_task(void *pvParameters)
     #ifdef CONFIG_MB_COMM_MODE_TCP
             tcp_master_operation_func(NULL);
     #else   
-            master_operation_func(NULL);
+            // master_operation_func(NULL);
     #endif
 #endif
         }
@@ -784,4 +784,5 @@ void send_command_task(void *pvParameters)
     #endif
 #endif
     }
+    vTaskDelete(NULL);
 }
